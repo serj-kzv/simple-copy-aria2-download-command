2199f273bf852caf9c47c6d0ba4e8e5674ddd372
null <null>
2025-04-27 23:54:30 +0300

    fix copy to buffer 4

--- src/changelog/migration2Fn.js
+++ src/changelog/migration2Fn.js
@@ -4,7 +4,7 @@ const migration2Fn = async (config) => {
     const options = config[Constants.option.options];
     if (options.length === 1 && options[0].name === undefined) {
         options[0][Constants.option.name] = 'common';
-        options[0][Constants.option.commandTemplate] = 'aria2 -c -s 4 -x 4 -k 1M --max-tries=0 %h %u';
+        options[0][Constants.option.commandTemplate] = 'aria2 -c -s4 --max-tries=0 %h %u';
     }
 };
 
