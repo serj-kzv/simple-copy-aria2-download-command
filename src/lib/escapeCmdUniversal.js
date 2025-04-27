a89d534ec26c406195ea0483955ec3ea73eac5b0
null <null>
2025-04-27 19:08:55 +0300

    up build

--- src/lib/escapeCmdUniversal.js
+++ src/lib/escapeCmdUniversal.js
@@ -9,17 +9,15 @@ const escapeCmdUniversal = (str, platforms = [PLATFORM.AUTO]) => {
         console.debug('escapeCmdUniversal is off');
         return str;
     }
-    if (platforms.includes(PLATFORM.AUTO)) {
-        console.debug('escapeCmdUniversal is on for:', platforms);
+    console.debug('escapeCmdUniversal is on for:', platforms);
 
-        if (platforms.includes(PLATFORM.WINDOWS)
-            && navigator.platform.indexOf('Win') >= 0) {
-            console.debug('escapeCmdUniversal current platform is:', PLATFORM.WINDOWS);
-            return str.replace(/%/g, '%%').replace(/([&<>|^"()!])/g, '^$1');
-        } else if (platforms.includes(PLATFORM.LINUX)) {
-            console.debug('escapeCmdUniversal current platform is:', PLATFORM.LINUX);
-            return `'${str.replace(/'/g, `'\\''`)}'`;
-        }
+    if (platforms.includes(PLATFORM.AUTO) || platforms.includes(PLATFORM.WINDOWS)
+        && navigator.platform.indexOf('Win') >= 0) {
+        console.debug('escapeCmdUniversal current platform is:', PLATFORM.WINDOWS);
+        return str.replace(/%/g, '%%').replace(/([&<>|^"()!])/g, '^$1');
+    } else if (platforms.includes(PLATFORM.AUTO) || platforms.includes(PLATFORM.LINUX)) {
+        console.debug('escapeCmdUniversal current platform is:', PLATFORM.LINUX);
+        return `'${str.replace(/'/g, `'\\''`)}'`;
     }
 };
 
