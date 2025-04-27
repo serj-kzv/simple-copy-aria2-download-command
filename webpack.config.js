d7e39f48216ed12bdca59102803e3ee420afac3d
null <null>
2025-04-27 22:21:45 +0300

    make elements depend on media type, fix 3

--- webpack.config.js
+++ webpack.config.js
@@ -12,67 +12,69 @@ const dist = path.resolve(__dirname, 'dist');
 const context = path.resolve(__dirname, 'src');
 const toType = 'template';
 
-export default ({envMode}, {mode}) => ((async () => {
-    if (envMode === 'pack') {
-        await webExt.cmd.build({
-            sourceDir: 'dist',
-            artifactsDir: 'build',
-            overwriteDest: true,
-            verbose: true,
-            ignoreFiles: ['**/node_modules/**', '*.log'],
-            logLevel: 'info',
-            shouldExitProgram: false
-        });
-    }
-
-    return {
-        mode,
-        entry: {
-            background: './src/background.js',
-            content: './src/content.js',
-            option: './src/option/option.js'
-        },
-        output: {path: dist, filename: '[name].js'},
-        resolve: {modules: [context, 'node_modules']},
-        devtool: mode === 'production' ? false : 'source-map',
-        target: 'web',
-        watchOptions: {ignored: /node_modules/, poll: 500},
-        module: {
-            rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {presets: [['@babel/preset-env', {targets: {firefox}}]]}
+export default ({envMode}, {mode}) => ({
+    mode,
+    entry: {
+        background: './src/background.js',
+        content: './src/content.js',
+        option: './src/option/option.js'
+    },
+    output: {path: dist, filename: '[name].js'},
+    resolve: {modules: [context, 'node_modules']},
+    devtool: mode === 'production' ? false : 'source-map',
+    target: 'web',
+    watchOptions: {ignored: /node_modules/, poll: 500},
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {presets: [['@babel/preset-env', {targets: {firefox}}]]}
+            }
+        }]
+    },
+    optimization: mode === 'production' ? {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 2024,
+                compress: {
+                    drop_console: true,
+                    drop_debugger: true,
+                    pure_funcs: ['console.log', 'console.debug', 'console.warn', 'console.info', 'console.error']
                 }
-            }]
-        },
-        optimization: mode === 'production' ? {
-            minimize: true,
-            minimizer: [new TerserPlugin({
-                parallel: true,
-                terserOptions: {
-                    ecma: 2024,
-                    compress: {
-                        drop_console: true,
-                        drop_debugger: true,
-                        pure_funcs: ['console.log', 'console.debug', 'console.warn', 'console.info', 'console.error']
-                    }
-                }
-            })]
-        } : {},
-        plugins: [
-            new CleanWebpackPlugin(),
-            new CopyWebpackPlugin({
-                patterns: [
-                    {from: '_locales/**', to: '[path][name][ext]', context},
-                    {from: 'assets/**', to: '[name][ext]', context},
-                    {from: 'manifest.json', to: '[name][ext]', context, toType},
-                    {from: 'icon.svg', to: '[name][ext]', context, toType},
-                    {from: 'option/option.css', to: '[name][ext]', context, toType},
-                    {from: 'option/option.html', to: '[name][ext]', context, toType}
-                ]
-            })
-        ]
-    };
-})());
+            }
+        })]
+    } : {},
+    plugins: [
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: '_locales/**', to: '[path][name][ext]', context},
+                {from: 'assets/**', to: '[name][ext]', context},
+                {from: 'manifest.json', to: '[name][ext]', context, toType},
+                {from: 'icon.svg', to: '[name][ext]', context, toType},
+                {from: 'option/option.css', to: '[name][ext]', context, toType},
+                {from: 'option/option.html', to: '[name][ext]', context, toType}
+            ]
+        }),
+        {
+            apply: mode === 'production' && envMode === 'pack' ? (compiler) => {
+                compiler.hooks.done.tap('WebExtPackPlugin', async () => {
+                    await webExt.cmd.build({
+                        sourceDir: 'dist',
+                        artifactsDir: 'build',
+                        overwriteDest: true,
+                        verbose: true,
+                        ignoreFiles: ['**/node_modules/**', '*.log'],
+                        logLevel: 'info',
+                        shouldExitProgram: false
+                    });
+                });
+            } : () => {
+            }
+        }
+    ]
+});
